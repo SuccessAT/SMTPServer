6aee7a629b2f192af25777643418f1e1c3408b37
SuccessAT <agboyinusuccess@gmail.com>
2026-01-22 00:09:37 +0100

    Remove env.txt, add .gitignore, Procfile, and render.yaml for deployment configuration

--- flask_app.py
+++ flask_app.py
@@ -255,4 +255,6 @@ def internal_error(e):
 if __name__ == '__main__':
     # This is for local testing only
     # PythonAnywhere uses WSGI
-    app.run(debug=False, host='0.0.0.0', port=5000)
+    # app.run(debug=False, host='0.0.0.0', port=5000)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(debug=False, host='0.0.0.0', port=port)
